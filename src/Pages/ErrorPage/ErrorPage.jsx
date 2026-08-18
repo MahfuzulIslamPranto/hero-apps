83ff4d91cafc809ecab10290ccea2ac33b351ef4
MahfuzulIslamPranto <pranto.uiu.bd@gmail.com>
2026-08-18 12:12:25 +0600

    assets added

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className='text-red'>Page not Found!</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Routers/Routers.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import Root from '../Pages/Root/Root';
+import Home from '../Pages/Home/Home'
+
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        Component: Root,
+        element: <ErrorPage></ErrorPage>,
+        children:[
+            {
+                index: true,
+                pat: '/',
+                Component: Home
+            }
+        ]
+    }
+])
