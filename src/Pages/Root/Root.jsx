f8533a42ba1407d7eedd4e03f315f398ff107498
MahfuzulIslamPranto <pranto.uiu.bd@gmail.com>
2026-08-30 12:51:47 +0600

    logo fixed on tab bar

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,19 +1,19 @@
-import React, { Suspense } from 'react';
-import Navbar from '../../Components/Header/Navbar';
-import { Outlet } from 'react-router';
-import Footer from '../../Components/Header/Footer/Footer';
-import Loading from '../Loading/Loading';
+import React, { Suspense } from "react";
+import Navbar from "../../Components/Header/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../../Components/Header/Footer/Footer";
+import Loading from "../Loading/Loading";
 
 const Root = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Suspense fallback={<Loading></Loading>}>
-                <Outlet></Outlet>
-            </Suspense>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Suspense fallback={<Loading></Loading>}>
+        <Outlet></Outlet>
+      </Suspense>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
